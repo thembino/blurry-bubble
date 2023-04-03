5292e306f96870ee13f243922c3429b7dd2e06d8
thembino <38435788+thembino@users.noreply.github.com>
2023-04-04 01:02:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# blurry-bubble
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-aukxvg)
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+const blob = document.getElementById('blob');
+
+document.body.onpointermove = (event) => {
+  const { clientX, clientY } = event;
+  blob.animate(
+    {
+      left: `${clientX}px`,
+      top: `${clientY}px`,
+    },
+    {
+      duration: 2000,
+      fill: 'forwards',
+    }
+  );
+};
